18722b7d9123136b402b4784224eb780a1d560eb
pro-masud <promasudbd@gmail.com>
2025-08-04 22:29:02 +0600

    feat: corpx home one url set successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LayoutOne from './components/LayoutOne'
 import HeroOne from './pages/HomeOne/HeroOne'
+import About from './pages/About'
+import OurProject from './pages/OurProject'
+import ProjectDetails from './pages/ProjectDetails'
+import Team from './pages/Team'
+import TeamDetails from './pages/TeamDetails'
+import OurPricing from './pages/OurPricing'
+import OurFaq from './pages/OurFaq'
+import Service from './pages/Service'
+import ServiceDetails from './pages/ServiceDetails'
+import BlogGrid from './pages/BlogGrid'
+import BlogStandard from './pages/BlogStandard'
+import BlogDetails from './pages/BlogDetails'
+import ContactUs from './pages/ContactUs'
 
 function App() {
   return (
@@ -10,15 +23,19 @@ function App() {
         {/* Home One Layout Routes */}
         <Route path="/" element={<LayoutOne />}>
           <Route index element={<HeroOne />} />
-          <Route path='about' element={<HeroOne />} />
-          <Route path='service-single' element={<HeroOne />} />
-          <Route path='service-details' element={<HeroOne />} />
-          <Route path='projects' element={<HeroOne />} />
-          <Route path='project-details' element={<HeroOne />} />
-          <Route path='blog' element={<HeroOne />} />
-          <Route path='blog-details' element={<HeroOne />} />
-          <Route path='blog-single' element={<HeroOne />} />
-          <Route path='contact' element={<HeroOne />} />
+          <Route path='about' element={<About />} />
+          <Route path='project' element={<OurProject />} />
+          <Route path='project-details' element={<ProjectDetails />} />
+          <Route path='team' element={<Team />} />
+          <Route path='team-details' element={<TeamDetails />} />
+          <Route path='pricing' element={<OurPricing />} />
+          <Route path='faq' element={<OurFaq />} />
+          {/* <Route path='404' element={<notFount404 />} /> */}
+          <Route path='service' element={<Service />} />
+          <Route path='blog' element={<BlogGrid />} />
+          <Route path='service-standard' element={<BlogStandard />} />
+          <Route path='service-details' element={<BlogDetails />} />
+          <Route path='contact' element={<ContactUs />} />
         </Route>
       </Routes>
     </Router>
