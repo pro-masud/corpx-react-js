61a048ae8b14bf2daaa1b6955734418e1d7ca9c1
pro-masud <promasudbd@gmail.com>
2025-08-07 00:15:39 +0600

    feat: marquee section dynamic complete

---
+++ src/data/marquee.js
@@ -0,0 +1,24 @@
+const marquee = [
+  {
+    "text": "Global business",
+    "style": "style2",
+    "iconType": "line"
+  },
+  {
+    "text": "Business solutions",
+    "style": "style1",
+    "iconType": "dot"
+  },
+  {
+    "text": "Startup solution",
+    "style": "style2",
+    "iconType": "line"
+  },
+  {
+    "text": "Business grow",
+    "style": "style1",
+    "iconType": "dot"
+  }
+]
+
+export default marquee;
--- src/pages/HomeOne/HeroOne.jsx
+++ src/pages/HomeOne/HeroOne.jsx
@@ -3,6 +3,7 @@ import Service from '../../components/HomeOne/Service'
 import About from '../../components/HomeOne/About'
 import Achievement from '../../components/HomeOne/Achievement'
 import WCU from '../../components/HomeOne/WCU'
+import Marquee from '../../components/HomeOne/Marquee'
 
 const HeroOne = () => {
   return (
@@ -12,6 +13,7 @@ const HeroOne = () => {
         <About />
         <Achievement />
         <WCU />
+        <Marquee />
     </>
   )
 }
