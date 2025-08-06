3ba040eed0e53a720895abb61d50630dd5e5193b
pro-masud <promasudbd@gmail.com>
2025-08-07 00:51:12 +0600

    feat: project problem issue solve it

--- src/components/HomeOne/Marquee.jsx
+++ src/components/HomeOne/Marquee.jsx
@@ -9,16 +9,19 @@ const Marquee = () => {
                 <div className="scrolling-wrap">
                     <div className="comm style1">
                         {marquee.map((singleMarquee, index) => 
-                            <>
-                                <div className={`cmn-textslide ${singleMarquee.style}`}>{singleMarquee.text}</div>
-                                <div className={singleMarquee.iconType}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="54" viewBox="0 0 50 54" fill="none">
-                                        <path
-                                            d="M29.703 54H19.802V37.5L5.44554 45L0 36L14.8515 26.5L0 17.5L5.44554 8.5L19.802 17.5V0H29.703V17.5L44.5545 8.5L50 17.5L35.6436 27.5L50 37.5L44.5545 45L29.703 36V54Z"
-                                            fill="#AEC0C9"></path>
-                                    </svg>
+                            <React.Fragment key={index}>
+                                <div className={`cmn-textslide ${singleMarquee.style}`}>
+                                {singleMarquee.text}
                                 </div>
-                            </>
+                                <div className={singleMarquee.iconType}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="54" viewBox="0 0 50 54" fill="none">
+                                    <path
+                                    d="M29.703 54H19.802V37.5L5.44554 45L0 36L14.8515 26.5L0 17.5L5.44554 8.5L19.802 17.5V0H29.703V17.5L44.5545 8.5L50 17.5L35.6436 27.5L50 37.5L44.5545 45L29.703 36V54Z"
+                                    fill="#AEC0C9"
+                                    />
+                                </svg>
+                                </div>
+                            </React.Fragment>
                         )}                        
                     </div>
                 </div>
--- src/components/HomeOne/Project.jsx
+++ src/components/HomeOne/Project.jsx
@@ -5,36 +5,28 @@ import projects from '../../data/projects'
 const Project = () => {
   return (
     <>
-    <section class="project-section section-padding bg-color3 fix">
-        <div class="container">
-            <div class="project-wrapper style3">
-                {/* <div class="section-title mxw-565">
-                    <span class="subtitle text-white wow fadeInUp"><img src="assets/img/icon/subTitleIconWhite.svg" alt="icon" />Our Portfolio</span>
-                    <h2 class="text-white wow fadeInUp" data-wow-delay=".3s">Professionals Using Our Business Solutions</h2>
-                </div> */}
+    <section className="project-section section-padding bg-color3 fix">
+        <div className="container">
+            <div className="project-wrapper style3">
                 <SectionTitle title="Professionals Using Our Business Solutions" subTitle="Our Portfolio" mainClass='mxw-565' colorClass="text-white" />
                 {projects.map((singleProject, index) => (
                     <div className="project-item style3" key={index}>
                         <div className="row g-4 d-flex align-items-center flex-wrap">
-                        <div className="col-md-6">
-                            <h3 className="project-name">{singleProject.title}</h3>
-                        </div>
-                        <div className="col-md-3">
-                            <div className="hover-image position-absolute overflow-hidden rounded-20">
-                            <div className="team-image">
-                                <img
-                                src={singleProject.image}
-                                className="img-fluid w-100"
-                                alt="thumb"
-                                />
+                            <div className="col-md-6">
+                                <h3 className="project-name">{singleProject.title}</h3>
                             </div>
+                            <div className="col-md-3">
+                                <div className="hover-image position-absolute overflow-hidden rounded-20">
+                                    <div className="team-image">
+                                        <img src={singleProject.image} className="img-fluid w-100" alt="thumb" />
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-md-3 d-flex justify-content-end">
-                            <div className="project-link">
-                            <a href={singleProject.link}>{singleProject.linkText}</a>
+                            <div className="col-md-3 d-flex justify-content-end">
+                                <div className="project-link">
+                                    <a href={singleProject.link}>{singleProject.linkText}</a>
+                                </div>
                             </div>
-                        </div>
                         </div>
                     </div>
                 ))}
