8774d435fff38424e359c49c93713f4c4d3dd6ab
pro-masud <promasudbd@gmail.com>
2025-08-06 09:03:45 +0600

    feat: counter components creation dynamic complete

--- src/components/HomeOne/Achievement.jsx
+++ src/components/HomeOne/Achievement.jsx
@@ -1,5 +1,6 @@
 import achievementImage from "../../assets/img/achievement/achievementThumb3_1.jpg";
 import achievementCircle from "../../assets/img/achievement/achievement-circle.png";
+import Counter from "../utils/Counter";
 
 const Achievement = () => {
   return (
@@ -12,24 +13,9 @@ const Achievement = () => {
                         <div className="row g-4">
                             <div className="col-xl-7">
                                 <div className="about-service-meta">
-                                    <div className="meta-box wow fadeInUp" data-wow-delay=".3s">
-                                        <div className="counter">
-                                            <span className="counter-number"> 20.5 </span> K
-                                        </div>
-                                        <p className="text">Projects Done</p>
-                                    </div>
-                                    <div className="meta-box wow fadeInUp" data-wow-delay=".6s">
-                                        <div className="counter">
-                                            <span className="counter-number"> 100.5 </span> K
-                                        </div>
-                                        <p className="text">Happy Clients</p>
-                                    </div>
-                                    <div className="meta-box wow fadeInUp" data-wow-delay=".8s">
-                                        <div className="counter">
-                                            <span className="counter-number"> 150.5 </span> K
-                                        </div>
-                                        <p className="text">Team Members</p>
-                                    </div>
+                                    <Counter number="20.50" suffix="K" text="Projects Done"  delay=".4s" />
+                                    <Counter number="100.50" suffix="K" text="Happy Clients"  delay=".6s" />
+                                    <Counter number="150.50" suffix="K" text="Team Members"  delay=".8s" />
                                 </div>
                             </div>
                             <div className="col-xl-5 d-flex justify-content-end">
---
+++ src/components/utils/Counter.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import Odometer from "react-odometerjs";
+import "odometer/themes/odometer-theme-default.css";
+import { useInView } from "react-intersection-observer";
+
+const Counter = ({ number, suffix = '', text, delay = '.8s' }) => {
+
+    const [odometerValue, setOdometerValue] = useState(0);
+    const [ref, inView] = useInView({
+        triggerOnce: true,
+        threshold: 0.3,
+    });
+
+    useEffect(() => {
+        if (inView) {
+        setTimeout(() => {
+            setOdometerValue(parseFloat(number).toFixed(2));
+        }, 1000);
+        }
+    }, [inView, number]);
+    
+  return (
+    <>
+        <div ref={ref} className="meta-box wow fadeInUp" data-wow-delay={delay}>
+            <div className="counter">
+                <Odometer  value={odometerValue} format="(,ddd).ddd" /> {suffix}
+            </div>
+            <p className="text">{text}</p>
+        </div>
+    </>
+  )
+}
+
+export default Counter
