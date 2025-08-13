fb70081a773329904abef1eaa8cf131773190c66
pro-masud <promasudbd@gmail.com>
2025-08-14 00:32:39 +0600

    feat: team one section dynamic complete

--- src/components/HomeTwo/Team.jsx
+++ src/components/HomeTwo/Team.jsx
@@ -5,31 +5,31 @@ import teams from '../../data/teams'
 const Team = () => {
   return (
     <>
-        <section class="team-member-section section-padding fix">
-            <div class="container">
-                <div class="team-member-wrapper style3">
+        <section className="team-member-section section-padding fix">
+            <div className="container">
+                <div className="team-member-wrapper style3">
                     <SectionTitleTwo title="Dedicated Expert Team" subTitle="Our Experts" mainClass='text-center mx-auto' />
-                    <div class="row g-4">
+                    <div className="row g-4">
                         {teams.map((singleTeam, index) =>
-                            <div key={index} class="col-xl-4 col-md-6">
-                                <div class="team-card style3 wow fadeInUp" data-wow-delay=".3s">
-                                    <div class="team-thumb">
+                            <div key={index} className="col-xl-4 col-md-6">
+                                <div className="team-card style3 wow fadeInUp" data-wow-delay=".3s">
+                                    <div className="team-thumb">
                                         <img src={singleTeam.image} alt="thumb" />
                                     </div>
-                                    <div class="team-content">
-                                        <div class="social-links">
-                                            <div class="social-media">
-                                                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                                                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                                                <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                                                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                                    <div className="team-content">
+                                        <div className="social-links">
+                                            <div className="social-media">
+                                                <a href="#"><i className="fa-brands fa-twitter"></i></a>
+                                                <a href="#"><i className="fa-brands fa-facebook-f"></i></a>
+                                                <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                                                <a href="#"><i className="fa-brands fa-linkedin-in"></i></a>
                                             </div>
-                                            <div class="plus-btn">
-                                                <i class="fa-solid fa-plus"></i>
+                                            <div className="plus-btn">
+                                                <i className="fa-solid fa-plus"></i>
                                             </div>
                                         </div>
                                         <h3><a href={singleTeam.profile_url}>{singleTeam.name}</a></h3>
-                                        <p class="text">{singleTeam.designation}</p>
+                                        <p className="text">{singleTeam.designation}</p>
                                     </div>
                                 </div>
                             </div>
