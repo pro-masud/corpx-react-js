6313b3188d983863530f587e2b6fb6a85052a988
pro-masud <promasudbd@gmail.com>
2025-08-28 09:46:01 +0600

    feat: about three section dynamic complete

--- src/components/HomeThree/AboutThree.jsx
+++ src/components/HomeThree/AboutThree.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import { SingleCounter } from '../utils/Counter'
+import aboutShape1_1 from "../../assets/img/shape/aboutShape1_1.png";
+import aboutThumb1_1 from "../../assets/img/about/aboutThumb1_1.jpg";
+import aboutThumb1_2 from "../../assets/img/about/aboutThumb1_2.jpg";
+import aboutProfileThumb1_1 from "../../assets/img/about/aboutProfileThumb1_1.jpg";
+import SectionTitleTwo from '../utils/SectionTitleTwo';
+import { Link } from 'react-router-dom';
 
 const AboutThree = () => {
   return (
@@ -10,9 +16,9 @@ const AboutThree = () => {
                     <div className="row g-4">
                         <div className="col-lg-6">
                             <div className="about-thumb">
-                                <div className="thumb1 img-custom-anim-left wow" data-wow-duration="1.5s" data-wow-delay="0.3s"><img src="assets/img/about/aboutThumb1_1.jpg" alt="thumb" /></div>
-                                <div className="thumb2 img-custom-anim-top wow" data-wow-duration="1.5s" data-wow-delay="0.5s"><img src="assets/img/about/aboutThumb1_2.jpg" alt="thumb" /></div>
-                                <div className="shape"><img src="assets/img/shape/aboutShape1_1.png" alt="shape" /></div>
+                                <div className="thumb1 img-custom-anim-left wow" data-wow-duration="1.5s" data-wow-delay="0.3s"><img src={aboutThumb1_1} alt="thumb" /></div>
+                                <div className="thumb2 img-custom-anim-top wow" data-wow-duration="1.5s" data-wow-delay="0.5s"><img src={aboutThumb1_2} alt="thumb" /></div>
+                                <div className="shape"><img src={aboutShape1_1} alt="shape" /></div>
                                 <div className="counter-box">
                                     <h2><SingleCounter value="25" />k+</h2>
                                     <p>Years Experience</p>
@@ -21,16 +27,9 @@ const AboutThree = () => {
                         </div>
                         <div className="col-lg-6">
                             <div className="about-content">
-                                <div className="section-title">
-                                    <span className="subtitle wow"><img
-                                            src="assets/img/icon/subTitleIcon.svg" alt="icon" />About Our Company</span>
-                                    <h2 className="splt-txt wow fadeInUp" data-wow-delay=".3s">Strong Organization For Corporate Operations.
-                                    </h2>
-                                    <p className="text wow fadeInUp" data-wow-delay=".5s">There are many variations of passages of
+                                 <SectionTitleTwo title="Strong Organization For Corporate Operations." subTitle="About Our Company" mainClass='mb-0' desc="There are many variations of passages of
                                         Lorem Ipsum available, but the majority have suffered alteration in some form, by
-                                        injected humor, or randomized words which don't look even slightly believable.</p>
-                                </div>
-
+                                        injected humor, or randomized words which don't look even slightly believable." />
                                 <div className="checklist-wrapper">
                                     <ul className="checklist wow fadeInUp" data-wow-delay="0.3s">
                                         <li><i className="fa-solid fa-check"></i> Innovative solutions</li>
@@ -44,11 +43,11 @@ const AboutThree = () => {
 
                                 <div className="contact-meta">
                                     <div className="btn-wrapper wow fadeInUp" data-wow-delay="0.3s">
-                                        <a className="theme-btn" href="about.html"> Discover more <i
-                                                className="fa-sharp fa-regular fa-arrow-up-right"></i></a>
+                                        <Link className="theme-btn" to="/about"> Discover more <i
+                                                className="fa-sharp fa-regular fa-arrow-up-right"></i></Link>
                                     </div>
                                     <div className="profile-meta wow fadeInUp" data-wow-delay="0.5s">
-                                        <div className="thumb"><img src="assets/img/about/aboutProfileThumb1_1.jpg" alt="thumb" />
+                                        <div className="thumb"><img src={aboutProfileThumb1_1} alt="thumb" />
                                         </div>
                                         <div className="content">
                                             <div className="name">Marvin donald</div>
